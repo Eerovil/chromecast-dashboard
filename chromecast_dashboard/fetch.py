673419415a8882538fd62fa1f57e95bc250dc2bf
Eero Vilpponen <eero.vilpponen@gmail.com>
2021-04-15 14:45:37 +0300

    split fetch and server

---
+++ chromecast_dashboard/fetch.py
@@ -0,0 +1,128 @@
+# coding: utf-8
+import os
+import json
+import requests
+import sys
+
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+creds = None
+albums = []
+try:
+    with open('albums.json', 'r') as album_file:
+        albums = json.load(album_file)
+except:
+    print("no albums")
+persist_lock = False
+
+
+def persist_cache():
+    global persist_lock
+    if persist_lock:
+        return
+    persist_lock = True
+    try:
+        with open('albums.json', 'w') as f:
+            json.dump(albums, f)
+    except Exception as e:
+        print(e)
+
+    persist_lock = False
+
+
+def init_oauth():
+    SCOPES = [
+        "https://www.googleapis.com/auth/photoslibrary.readonly",
+        "https://www.googleapis.com/auth/photoslibrary.sharing"
+    ]
+    global creds
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+
+def get_all_albums():
+    global creds
+    headers = {'Authorization': 'Bearer {}'.format(creds.token)}
+    response = requests.get(
+        'https://photoslibrary.googleapis.com/v1/albums',
+        headers=headers
+    )
+    for album_data in response.json()["albums"]:
+        if album_data['title'] in config['ALBUMS']:
+            album_data['cached_media_items'] = []
+            album_data['config'] = config['ALBUMS'][album_data['title']]
+            albums.append(album_data)
+
+
+def fetch_media_item(album, index=0):
+    print("Fetching index %s from album %s", index, album['title'])
+    global creds
+    headers = {'Authorization': 'Bearer {}'.format(creds.token)}
+    page_token = ""
+    counter = 0
+    ret = None
+    if len(album['cached_media_items']) > index:
+        ret = album['cached_media_items'][index]
+        print("Found in cache!")
+        print(ret['id'])
+        return ret
+    elif len(album['cached_media_items']) > 0 and album.get('cached_page_token', None):
+        print("Cache exists, but not long enough.")
+        page_token = album['cached_page_token']
+        counter += len(album['cached_media_items'])
+        index -= counter
+    while index >= 0:
+        print("Fetching page.. (items %s to %s)", counter, counter + 100)
+        response = requests.post(
+            'https://photoslibrary.googleapis.com/v1/mediaItems:search',
+            headers=headers,
+            data={
+                "albumId": album["id"],
+                "pageSize": "100",
+                "pageToken": page_token,
+            }
+        )
+        data = response.json()
+        if "mediaItems" not in data:
+            print(json.dumps(data, indent=4))
+            return Response("error (quota?)", status=500)
+        counter += 100
+        if len(album['cached_media_items']) < counter:
+            album['cached_media_items'] += data["mediaItems"]
+        if index < 100:
+            ret = data["mediaItems"][index]
+            break
+        page_token = data["nextPageToken"]
+        album['cached_page_token'] = page_token
+        persist_cache()
+        index -= 100
+
+    print("Found mediaitem! (cache size %s)", len(album['cached_media_items']))
+    print("%s", ret['id'])
+    return ret
+
+
+if __name__ == '__main__':
+    init_oauth()
+    get_all_albums()
+    for album in albums:
+        if len(sys.argv) > 1:
+            fetch_media_item(album, int(sys.argv[1]) - 1)
+            break
+        fetch_media_item(album, int(album['mediaItemsCount']) - 1)
--- chromecast_dashboard/main.py
+++ chromecast_dashboard/main.py
@@ -1,15 +1,11 @@
 # coding: utf-8
-import os
 import json
 from flask import Flask, flash, request, redirect, send_from_directory
-import requests
 import random
 
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-
 import logging
+
+from requests.models import Response
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
@@ -18,6 +14,12 @@ with open('config.json', 'r') as config_file:
     app.config.update(json.load(config_file))
 creds = None
 albums = []
+try:
+    with open('albums.json', 'r') as album_file:
+        albums = json.load(album_file)
+except:
+    app.logger.info("no albums")
+    raise AttributeError()
 
 
 @app.route('/', methods=['GET'])
@@ -54,7 +56,7 @@ def randomize_image():
     """
     total_media_count = 0
     for album in albums:
-        total_media_count += int(album["mediaItemsCount"]) - 1
+        total_media_count += len(album["cached_media_items"]) - 1
     
     # total_media_count = 200
     if len(albums[0]['cached_media_items']) == 0:
@@ -78,88 +80,18 @@ def randomize_image():
     app.logger.error("Failed to get random image!")
     return None
 
-def init_oauth():
-    SCOPES = [
-        "https://www.googleapis.com/auth/photoslibrary.readonly",
-        "https://www.googleapis.com/auth/photoslibrary.sharing"
-    ]
-    global creds
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-
-def get_all_albums():
-    global creds
-    headers = {'Authorization': 'Bearer {}'.format(creds.token)}
-    response = requests.get(
-        'https://photoslibrary.googleapis.com/v1/albums',
-        headers=headers
-    )
-    for album_data in response.json()["albums"]:
-        if album_data['title'] in app.config['ALBUMS']:
-            album_data['cached_media_items'] = []
-            album_data['config'] = app.config['ALBUMS'][album_data['title']]
-            albums.append(album_data)
-
 
 def fetch_media_item(album, index=0):
     app.logger.info("Fetching index %s from album %s", index, album['title'])
-    global creds
-    headers = {'Authorization': 'Bearer {}'.format(creds.token)}
-    page_token = ""
-    counter = 0
     ret = None
     if len(album['cached_media_items']) > index:
         ret = album['cached_media_items'][index]
         app.logger.info("Found in cache!")
         app.logger.debug(ret['id'])
         return ret
-    elif len(album['cached_media_items']) > 0 and album.get('cached_page_token', None):
-        app.logger.info("Cache exists, but not long enough.")
-        page_token = album['cached_page_token']
-        counter += len(album['cached_media_items'])
-        index -= counter
-    while index >= 0:
-        app.logger.info("Fetching page.. (items %s to %s)", counter, counter + 100)
-        response = requests.post(
-            'https://photoslibrary.googleapis.com/v1/mediaItems:search',
-            headers=headers,
-            data={
-                "albumId": album["id"],
-                "pageSize": "100",
-                "pageToken": page_token,
-            }
-        )
-        data = response.json()
-        if "mediaItems" not in data:
-            app.logger.error(json.dumps(data, indent=4))
-        counter += 100
-        if len(album['cached_media_items']) < counter:
-            album['cached_media_items'] += data["mediaItems"]
-        if index < 100:
-            ret = data["mediaItems"][index]
-            break
-        page_token = data["nextPageToken"]
-        album['cached_page_token'] = page_token
-        index -= 100
 
-    app.logger.info("Found mediaitem! (cache size %s)", len(album['cached_media_items']))
-    app.logger.debug("%s", ret['id'])
     return ret
 
 
 if __name__ == '__main__':
-    init_oauth()
-    get_all_albums()
     app.run(host="0.0.0.0", port="5010")
